29202b90e6efe473c2885a9c1da3a398ea7fcc74
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-01-19 21:55:54 +0300

    added Read, Delete and Create operation on firestore database

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,11 +3,32 @@ import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { useState, useEffect } from "react";
 import { db } from "../../../config/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, deleteDoc, doc } from "firebase/firestore";
+import loadingImage from "../../assets/loading.gif";
 
 const Home = () => {
   const [cities, setCities] = useState([]);
+  const [cityInfo, setCityInfo] = useState({ cityName: "", Country: "", Continent: "" });
+
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  //   handle frcging f data
+  async function getCities() {
+    try {
+      const data = await getDocs(collection(db, "cities"));
+      const citiesData = data.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCities(citiesData);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // handle signout
   const handleSignOut = async () => {
     try {
       await signOut(auth);
@@ -17,40 +38,97 @@ const Home = () => {
     }
   };
 
+  //   function to  add new document
+  const handleCityInfo = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "cities"), cityInfo);
+
+      getCities();
+      alert("Added successfully");
+    } catch (error) {
+      console.log(error);
+    }
+    setCityInfo({ cityName: "", Country: "", Continent: "" });
+  };
+
   //   fetching data from db
 
   useEffect(() => {
-    async function getCities() {
-      try {
-        const data = await getDocs(collection(db, "cities"));
-        const citiesData = data.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setCities(citiesData);
-      } catch (error) {
-        console.log(error);
-      }
-    }
     getCities();
   }, []);
 
-  console.log(cities);
+  // function to delete a document from firestore database
+
+  const deleteCity = async (id) => {
+    const city = doc(db, "cities", id);
+    await deleteDoc(city);
+    getCities();
+  };
+
+  //    function to update a city information
+
   return (
     <div>
       <div>
         <button onClick={handleSignOut}>Logout</button>
       </div>
       <div>
+        <form onSubmit={handleCityInfo}>
+          <div>Create new city info</div>
+          <div>
+            <input
+              type="text"
+              placeholder="City Name"
+              value={cityInfo.cityName}
+              onChange={(e) => {
+                setCityInfo({ ...cityInfo, cityName: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Country Name"
+              value={cityInfo.Country}
+              onChange={(e) => {
+                setCityInfo({ ...cityInfo, Country: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Continent Name"
+              value={cityInfo.Continent}
+              onChange={(e) => {
+                setCityInfo({ ...cityInfo, Continent: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <button type="submit">Add Data</button>
+          </div>
+        </form>
+      </div>
+      <div></div>
+      <div>
         <h2>List of Cities with Countries & Continents</h2>
-        <ul>
-          {cities.map((city) => (
-            <li key={city.id}>
-              {" "}
-              City name :{city.cityName}, Country name : {city.Country}, located in {city.Continent}.
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <div>
+            <img src={loadingImage} alt="" />
+          </div>
+        ) : (
+          <ul>
+            {cities.map((city) => (
+              <li key={city.id}>
+                {" "}
+                City name :{city.cityName}, Country name : {city.Country}, located in {city.Continent}.{" "}
+                <button onClick={() => deleteCity(city.id)}>delete</button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div></div>
     </div>
